1ec9a5cf8e65c9bc421226986794218c59c0a81f
63A9F0EA7BB98050796B649E85481845 <info@rpdl.net>
2022-09-16 11:04:21 +0100

    fixed footer formatting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,8 +5,8 @@
 <!--        <a class="hover:text-slate-400" href="">Terms of Service</a>-->
 <!--        <p class="sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200/5"><a class="hover:text-slate-400" href="">Privacy Policy</a></p>-->
 <!--      </div>-->
-      <a class="hover:text-white transition duration-200" <a href = "https://rpdl.net/discord" > <img src="https://discordapp.com/api/guilds/911568616758050817/widget.png?style=shield" > </a>
-      <a class="hover:text-white transition duration-200" href="https://status.rpdl.net" target="_blank">Platform Status</a>
+      <a class="hover:text-white transition duration-200" <a href = "https://rpdl.net/discord" > <img src="https://discordapp.com/api/guilds/911568616758050817/widget.png?style=shield"></a>&nbsp;|&nbsp
+      <a class="hover:text-white transition duration-200" href="https://status.rpdl.net" target="_blank">Platform Status</a>&nbsp;|&nbsp
       <a class="hover:text-white transition duration-200" href="https://github.com/torrust/torrust">Powered by Torrust</a>
     </div>
   </div>
